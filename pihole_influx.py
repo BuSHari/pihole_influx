39ade3858c3792ac8ec4862f4b8fe084a95a985b
BuSHari <ibushari@outlook.com>
2018-09-07 14:11:55 +0300

    add environments

--- pihole_influx.py
+++ pihole_influx.py
@@ -10,14 +10,14 @@ import requests
 import time
 from influxdb import InfluxDBClient
 
-HOSTNAME = "ns-01" # Pi-hole hostname to report in InfluxDB for each measurement
-PIHOLE_API = "http://PI_HOLE_IP_ADDRESS_HERE/admin/api.php"
-INFLUXDB_SERVER = "127.0.0.1" # IP or hostname to InfluxDB server
-INFLUXDB_PORT = 8086 # Port on InfluxDB server
-INFLUXDB_USERNAME = "username"
-INFLUXDB_PASSWORD = "password"
-INFLUXDB_DATABASE = "piholestats"
-DELAY = 600 # seconds
+HOSTNAME = os.getenv['HOSTNAME', "ns-01"] # Pi-hole hostname to report in InfluxDB for each measurement
+PIHOLE_API = os.getenv['PIHOLE_API', "http://PI_HOLE_IP_ADDRESS_HERE/admin/api.php"]
+INFLUXDB_SERVER = os.getenv['INFLUXDB_SERVER', "127.0.0.1"] # IP or hostname to InfluxDB server
+INFLUXDB_PORT = os.getenv['INFLUXDB_PORT', "8086"] # Port on InfluxDB server
+INFLUXDB_USERNAME = os.getenv['INFLUXDB_USERNAME', "username"]
+INFLUXDB_PASSWORD = os.getenv['INFLUXDB_PASSWORD', "password"]
+INFLUXDB_DATABASE = os.getenv['INFLUXDB_DATABASE', "piholestats"]
+# DELAY = 600 # seconds
 
 def send_msg(domains_being_blocked, dns_queries_today, ads_percentage_today, ads_blocked_today):
 
@@ -47,7 +47,7 @@ def send_msg(domains_being_blocked, dns_queries_today, ads_percentage_today, ads
 
 if __name__ == '__main__':
         while True:
-          time.sleep(DELAY)
+          # time.sleep(DELAY)
           api = requests.get(PIHOLE_API) # URI to pihole server api
           API_out = api.json()
           domains_being_blocked = (API_out['domains_being_blocked'])
